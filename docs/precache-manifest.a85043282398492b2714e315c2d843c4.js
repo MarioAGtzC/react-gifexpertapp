38ea4ef1ff866c18b41fc6b117fc377493cee1f6
Marioagtzc <marioagtzc@hotmail.com>
2020-09-15 15:57:36 -0500

    added docs

---
+++ docs/precache-manifest.a85043282398492b2714e315c2d843c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2590b45f273b3a4aebaedcd54d41df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96bb22d2962daa057520",
+    "url": "/static/css/main.1041f713.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bb22d2962daa057520",
+    "url": "/static/js/main.f1474289.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
